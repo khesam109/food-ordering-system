dc047cd432efb20bd9637545775b6145352f637a
khesam109 <khesam109@gmail.com>
2022-08-04 00:45:43 -0700

    create common ValueObject and DomainEvent

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.food.ordering.system.domain.entity;
 
 import java.util.Objects;
 
+//TODO: Is it possible to restrict ID to sub-classes of BaseId?!
 public abstract class BaseEntity<ID> {
 
     private ID id;
---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,78 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public class Money {
+
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * NOTE: compareTo vs equals in BigDecimal
+     */
+    public boolean isGreaterThanZero() {
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    /**
+     * TODO: shouldn't need to check for being null of input param?
+     */
+    public boolean isGreaterThan(Money money) {
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    public Money add(Money money) {
+        return new Money(
+                setScale(
+                        this.amount.add(money.getAmount())
+                )
+        );
+    }
+
+    public Money subtract(Money money) {
+        return new Money(
+                setScale(
+                        this.amount.subtract(money.getAmount())
+                )
+        );
+    }
+
+    public Money multiply(int multiplier) {
+        return new Money(
+                setScale(
+                        this.amount.multiply(new BigDecimal(multiplier))
+                )
+        );
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount.equals(money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    /**
+     * NOTE: <a href="http://www.aligelenler.com/2014/10/using-fractional-numbers-in-java_14.html">
+     *     Using Fractional Numbers In Java
+     *     </a>
+     */
+    private BigDecimal setScale(BigDecimal input) {
+        return input.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
