236e7328f70ee77711094c1387369c721da4405f
khesam109 <khesam109@gmail.com>
2022-09-15 01:34:31 -0700

    add customer service and also db schema

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+    VALUES ('b5f83cc1-2dd2-43cd-95d6-8dd7ba0d2f64', 'user_1', 'First', 'User');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+    VALUES ('5cec7c36-3c93-425a-a520-b087d9dab4e4', 'user_2', 'Second', 'User');
