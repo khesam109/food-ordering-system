59748a5c028d183a4f110ae1ee334084555514c1
khesam109 <khesam109@gmail.com>
2022-11-15 09:09:19 -0800

    restaurant dataaccess implemented

---
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/OrderApprovalRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.food.ordering.system.restaurant.service.dataaccess.restaurant.adapter;
+
+import com.food.ordering.system.restaurant.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.restaurant.service.dataaccess.restaurant.repository.OrderApprovalJpaRepository;
+import com.food.ordering.system.restaurant.service.domain.entity.OrderApproval;
+import com.food.ordering.system.restaurant.service.domain.ports.output.repository.OrderApprovalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderApprovalRepositoryImpl implements OrderApprovalRepository {
+
+    private final OrderApprovalJpaRepository orderApprovalJpaRepository;
+    private final RestaurantDataAccessMapper restaurantDataAccessMapper;
+
+    @Autowired
+    public OrderApprovalRepositoryImpl(
+            OrderApprovalJpaRepository orderApprovalJpaRepository,
+            RestaurantDataAccessMapper restaurantDataAccessMapper
+    ) {
+        this.orderApprovalJpaRepository = orderApprovalJpaRepository;
+        this.restaurantDataAccessMapper = restaurantDataAccessMapper;
+    }
+
+    @Override
+    public OrderApproval save(OrderApproval orderApproval) {
+        return restaurantDataAccessMapper
+                .orderApprovalEntityToOrderApproval(
+                        orderApprovalJpaRepository.save(
+                                restaurantDataAccessMapper.orderApprovalToOrderApprovalEntity(orderApproval)
+                        )
+                );
+    }
+}
