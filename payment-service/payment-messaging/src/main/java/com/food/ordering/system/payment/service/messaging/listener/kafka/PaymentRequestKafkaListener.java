7f96f2d8650f4efe5fda473f8917362cdcbee2ba
khesam109 <khesam109@gmail.com>
2022-11-23 14:24:33 -0800

    fix kafka config

--- payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
+++ payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
@@ -33,7 +33,7 @@ public class PaymentRequestKafkaListener implements KafkaConsumer<PaymentRequest
 
     @Override
     @KafkaListener(id = "${kafka-consumer-config.payment-consumer-group-id}",
-            topics = "${payment-service.payment-request-topic name}")
+            topics = "${payment-service.payment-request-topic-name}")
     public void receive(
             @Payload List<PaymentRequestAvroModel> messages,
             @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
