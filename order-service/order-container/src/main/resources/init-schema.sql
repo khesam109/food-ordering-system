f46c591304d84667a66b60191a23891d25bfc1f3
khesam109 <khesam109@gmail.com>
2022-11-25 07:03:10 -0800

    initial implementation for outbox pattern

--- order-service/order-container/src/main/resources/init-schema.sql
+++ order-service/order-container/src/main/resources/init-schema.sql
@@ -54,15 +54,60 @@ ALTER TABLE "food_order".order_address
     ON DELETE CASCADE
     NOT VALID;
 
+DROP TYPE IF EXISTS "food_order".saga_status;
+CREATE TYPE "food_order".saga_status AS ENUM ('STARTED', 'FAILED', 'SUCCEEDED', 'PROCESSING', 'COMPENSATING', 'COMPENSATED');
+
+DROP TYPE IF EXISTS "food_order".outbox_status;
+CREATE TYPE "food_order".outbox_status AS ENUM ('STARTED', 'COMPLETED', 'FAILED');
+
+DROP TABLE IF EXISTS "food_order".payment_outbox CASCADE;
+CREATE TABLE "food_order".payment_outbox
+(
+    id uuid NOT NULL,
+    saga_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    payload jsonb NOT NULL,
+    outbox_status "food_order".outbox_status NOT NULL,
+    saga_status "food_order".saga_status NOT NULL,
+    order_status "food_order".order_status NOT NULL,
+    version integer NOT NULL,
+    CONSTRAINT payment_outbox_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX "payment_outbox_saga_status"
+    ON "food_order".payment_outbox
+    (type, outbox_status, saga_status);
+
+CREATE UNIQUE INDEX "payment_outbox_saga_id"
+    ON "food_order".payment_outbox
+        (type, saga_id, saga_status);
 
 
+DROP TABLE IF EXISTS "food_order".restaurant_approval_outbox CASCADE;
+CREATE TABLE "food_order".restaurant_approval_outbox
+(
+    id uuid NOT NULL,
+    saga_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    payload jsonb NOT NULL,
+    outbox_status "food_order".outbox_status NOT NULL,
+    saga_status "food_order".saga_status NOT NULL,
+    order_status "food_order".order_status NOT NULL,
+    version integer NOT NULL,
+    CONSTRAINT restaurant_approval_outbox_pkey PRIMARY KEY (id)
+);
 
+CREATE INDEX "restaurant_approval_outbox_saga_status"
+    ON "food_order".restaurant_approval_outbox
+        (type, outbox_status, saga_status);
 
-
-
-
-
-
+CREATE UNIQUE INDEX "restaurant_approval_outbox_saga_id"
+    ON "food_order".restaurant_approval_outbox
+        (type, saga_id, saga_status);
 
 
 
